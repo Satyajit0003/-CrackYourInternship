a7590c992804ab5bc615308c4c2fcc9cf48d1119
Satyajit Sahoo <149392619+Satyajit0003@users.noreply.github.com>
2024-07-15 09:50:53 +0530

    Time: 0 ms (100%), Space: 42 MB (7.76%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void mergeSort(int[] nums,int si,int ei){
+        if(si>=ei){
+            return;
+        }
+        int mid=si+(ei-si)/2;
+        mergeSort(nums,si,mid);
+        mergeSort(nums,mid+1,ei);
+        merge(nums,si,mid,ei);
+    }
+    public void merge(int[] nums,int si,int mid,int ei){
+        int[] temp=new int[ei-si+1];
+        int k=0;
+        int i=si;
+        int j=mid+1;
+        while(i<=mid && j<=ei){
+            if(nums[i]<nums[j]){
+                temp[k++]=nums[i++];
+            }
+            else{
+                temp[k++]=nums[j++];
+            }
+        }
+        while(i<=mid){
+            temp[k++]=nums[i++];
+        }
+        while(j<=ei){
+            temp[k++]=nums[j++];
+        }
+        for(int l=0,m=si;l<temp.length;l++,m++){
+            nums[m]=temp[l];
+        }
+    }
+    public void sortColors(int[] nums) {
+        mergeSort(nums,0,nums.length-1);
+    }
+}
